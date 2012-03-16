7d392cdc62992729f7d383812666b03078a4d300
menotti@gmail.com <menotti@gmail.com@94e61729-cce0-ee49-765c-e3f32daa96b2>
2012-03-16 20:00:48 +0000

    MyHDL examples added

---
+++ trunk/myhdl/dff.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from myhdl import *
+from random import randrange
+
+def dff(q, d, clk):
+ 
+    @always(clk.posedge)
+    def logic():
+        q.next = d
+    return logic
+
+def tb_dff():
+ 
+    q, d, clk = [Signal(bool(0)) for i in range(3)]
+ 
+    dff_inst = dff(q, d, clk)
+ 
+    @always(delay(10))
+    def clkgen():
+        clk.next = not clk
+ 
+    @always(clk.negedge)
+    def stimulus():
+        d.next = randrange(2)
+ 
+    return dff_inst, clkgen, stimulus
+ 
+def simulate(timesteps):
+    tb = traceSignals(tb_dff)
+    sim = Simulation(tb)
+    sim.run(timesteps)
+
+def convert():
+    q, d, clk = [Signal(bool(0)) for i in range(3)]
+    toVerilog(dff, q, d, clk)
+ 
+simulate(2000)
+convert() 
