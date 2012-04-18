bdf9158d5c0b27e99283ca799509d34f9bca2dfa
menotti@gmail.com <menotti@gmail.com@94e61729-cce0-ee49-765c-e3f32daa96b2>
2012-04-18 20:13:43 +0000

    Directory structure change

--- trunk/myhdl/dff/dff.py
+++ trunk/myhdl/dff/dff.py
@@ -10,7 +10,7 @@ def dff(q, d, clk):
         q.next = d
     return logic
 
-def tb_dff():
+def t_dff():
  
     q, d, clk = [Signal(bool(0)) for i in range(3)]
  
@@ -27,13 +27,14 @@ def tb_dff():
     return dff_inst, clkgen, stimulus
  
 def simulate(timesteps):
-    tb = traceSignals(tb_dff)
+    tb = traceSignals(t_dff)
     sim = Simulation(tb)
     sim.run(timesteps)
 
 def convert():
     q, d, clk = [Signal(bool(0)) for i in range(3)]
     toVerilog(dff, q, d, clk)
+    toVHDL(dff, q, d, clk)
  
 simulate(2000)
 convert() 
