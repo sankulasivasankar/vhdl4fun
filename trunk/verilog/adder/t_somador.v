cea0356c32fefcb9d6f30a5e9946606f3507a4fc
menotti@gmail.com <menotti@gmail.com@94e61729-cce0-ee49-765c-e3f32daa96b2>
2012-11-09 16:32:00 +0000

    Verilog testbench fixed

--- trunk/verilog/adder/t_somador.v
+++ trunk/verilog/adder/t_somador.v
@@ -8,27 +8,28 @@ module t_somador;
 
 	parameter cycles = 8;
 
-	reg [4:0] data [0:cycles];
+	reg [4:0] data [0:cycles-1];
 
 	somador DUT (.i0(i0), .i1(i1), .ci(ci), .s(s), .co(co));
 
 	initial begin
-		data[1] = 5'b00000;
-		data[2] = 5'b00110;
-		data[3] = 5'b01010;
-		data[4] = 5'b01101;
-		data[5] = 5'b10010;
-		data[6] = 5'b10101;
-		data[7] = 5'b11001;
-		data[8] = 5'b11111;
+		data[0] = 5'b00000;
+		data[1] = 5'b00110;
+		data[2] = 5'b01010;
+		data[3] = 5'b01101;
+		data[4] = 5'b10010;
+		data[5] = 5'b10101;
+		data[6] = 5'b11001;
+		data[7] = 5'b11111;
 	end
 
 	initial begin
-		for (i = 0; i <= cycles; i = i + 1)
+		for (i = 0; i < cycles; i = i + 1)
 		begin
 			i0 = data[i][4];
 			i1 = data[i][3];
 			ci = data[i][2];
+			#1
 			if (s !== data[i][1])
 			begin
 				$display("bad sum value");
@@ -39,8 +40,8 @@ module t_somador;
 				$display("carry out value");
 				$finish;
 			end
-			#1;
 		end
+		$finish;
 	end 
 	
 
