68f8298eb6eded43feb1ead0b354ed7852a2654d
menotti@gmail.com <menotti@gmail.com@94e61729-cce0-ee49-765c-e3f32daa96b2>
2013-10-15 19:49:57 +0000

    Modules renamed

--- trunk/verilog/tribonacci_struc/tribonacci.v
+++ trunk/verilog/tribonacci_struc/tribonacci.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module fibonacci(clk, rst, s);
+module tribonacci(clk, rst, s);
 
 parameter width = 32;
 
@@ -10,21 +10,21 @@ input rst;
 output [width-1:0] s;
 
 
-reg16 #(.width(width))
+regn #(.width(width))
 	regX(
 	.clk(clk),
 	.rst(rst),
 	.d(s1),
 	.q(s0));
 
-reg16 #(.rst_value(1), .width(width)) 
+regn #(.rst_value(1), .width(width)) 
 	regY(
 	.clk(clk),
 	.rst(rst),
 	.d(s2),
 	.q(s1));
 
-reg16 #(.rst_value(1), .width(width)) 
+regn #(.rst_value(1), .width(width)) 
 	regZ(
 	.clk(clk),
 	.rst(rst),
@@ -37,7 +37,7 @@ endmodule
 
 
 
-module reg16(clk, rst, d, q);
+module regn(clk, rst, d, q);
 
 	parameter rst_value = 0;
 	parameter width = 16;
