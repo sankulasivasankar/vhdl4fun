5c313172f672102d4fab4106cf4e042c862d5fa9
viniciusnagura@caifas.com.br <viniciusnagura@caifas.com.br@94e61729-cce0-ee49-765c-e3f32daa96b2>
2013-11-07 23:54:17 +0000

    tribonacci_behav and t_tribonacci_behav added

---
+++ trunk/verilog/tribonacci_behav/tribonacci_behav.v
@@ -0,0 +1,30 @@
+module tribonacci_behav(clk, rst, s);
+
+	input clk, rst;
+	output [31:0] s;
+
+	reg [31:0] reg_0;
+	reg [31:0] reg_1;
+	reg [31:0] reg_2;
+
+
+	always @(posedge rst or posedge clk)
+	begin
+		if (rst == 1)
+		begin
+			reg_0 <= 32'b00000000000000000000000000000000;
+			reg_1 <= 32'b00000000000000000000000000000001;
+			reg_2 <= 32'b00000000000000000000000000000001;
+		end
+		else
+		begin
+			reg_0 <= reg_1;
+			reg_1 <= reg_2;
+			reg_2 <= reg_2 + reg_1 + reg_0;			
+			
+		end
+	end
+
+	assign s = reg_0;
+
+endmodule
