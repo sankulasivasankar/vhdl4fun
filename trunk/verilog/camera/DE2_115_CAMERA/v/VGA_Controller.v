e358b86f41fea6fbc65ffcbae771c16024675aeb
igorgbianchi@gmail.com <igorgbianchi@gmail.com@94e61729-cce0-ee49-765c-e3f32daa96b2>
2014-05-23 18:25:43 +0000

    Atualização

--- trunk/verilog/camera/DE2_115_CAMERA/v/VGA_Controller.v
+++ trunk/verilog/camera/DE2_115_CAMERA/v/VGA_Controller.v
@@ -25,7 +25,8 @@ module	VGA_Controller(	//	Host Side
 						puroG,
 						puroB,
 						padrao,
-						morfologico
+						morfologico,
+						bSobel
 							);
 
 //	Horizontal Parameter	( Pixel )
@@ -67,6 +68,12 @@ input [9:0] x2;
 input [9:0] y2;
 input [9:0] morfologico;
 
+reg valido; 
+reg [14:0] contador;
+reg reset_busca, ativo, check;
+reg reset_validacao;	
+input [9:0] bSobel;
+
 wire		[9:0]	mVGA_R;
 wire		[9:0]	mVGA_G;
 wire		[9:0]	mVGA_B;
@@ -82,14 +89,10 @@ input 				iZOOM_MODE_SW;
 
 //	Internal Registers and Wires
 reg		[12:0]		H_Cont, x1_search, x2_search, x1_search2, x2_search2;
-reg		[12:0]		V_Cont, x1_achou2, x2_achou2, y1_achou2, y2_achou2;
+reg		[12:0]		V_Cont, x1_achou, x2_achou, y1_achou, y2_achou, x1_achou2, x2_achou2, y1_achou2, y2_achou2;
 reg [10:0] OkLinha, LinhaCheck, OkLinha2, LinhaCheck2;
 reg [10:0] contadorBranco, contadorBranco2;
 reg achou, achou2;
-
-wire ativo;
-
-wire [12:0]x1_achou, x2_achou, y1_achou, y2_achou;
 
 wire	[12:0]		v_mask;
 
@@ -111,20 +114,7 @@ assign	mVGA_B	=	(	H_Cont>=X_START 	&& H_Cont<X_START+H_SYNC_ACT &&
 						?	iBlue	:	0;
 
 						
-Verificador verifica(
-			.Clk(iCLK),
-			.Rst(iRST_N),
-			.morfologico(morfologico),
-			.padrao(padrao),
-			.H_Cont(H_Cont),
-			.V_Cont(V_Cont),
-			.V_SYNC_TOTAL(V_SYNC_TOTAL),
-			.x1(x1_achou),
-			.x2(x2_achou),
-			.y1(y1_achou),
-			.y2(y2_achou),
-			.ativo(ativo)
-			);
+
 						
 always@(posedge iCLK or negedge iRST_N)
 	begin
@@ -137,12 +127,134 @@ always@(posedge iCLK or negedge iRST_N)
 				oVGA_SYNC <= 0;
 				oVGA_H_SYNC <= 0;
 				oVGA_V_SYNC <= 0;
+				achou <= 1'b0;
+				contadorBranco <= 0;
+				x1_search <= 0;
+				x2_search <= 300;
+				OkLinha <= 0;
+				LinhaCheck <= 0;
+				achou2 <= 1'b0;
+				contadorBranco2 <= 0;
+				x1_search2 <= 800;
+				x2_search2 <= 500;
+				OkLinha2 <= 0;
+				LinhaCheck2 <= 0;
+				ativo = 1'b0;
+				check = 1'b0;
+				reset_validacao <= 1'b0;
 			end
 		else
 			begin
-				if(padrao == 1'b1)begin 
-					if(ativo == 1'b1)begin 
+				if(reset_busca == 1'b1) begin
+					achou <= 1'b0;
+					achou2 <= 1'b0;
+					check <= 1'b0;
+					reset_validacao <= 1'b1;
+				end
+				if(valido == 1'b1) begin
+					check <= 1'b0;
+					ativo <= 1'b1;
+				end
+				if(padrao == 1'b1 && achou == 1'b0 && achou2 == 1'b0) begin
+					if(LinhaCheck == 80)begin
+						OkLinha <= 0;
+						LinhaCheck <= 0;
+					end
+					if(LinhaCheck2 == 80) begin
+						OkLinha2 <= 0;
+						LinhaCheck2 <= 0;
+					end				
+					if(H_Cont == x2_search)
+						LinhaCheck <= LinhaCheck + 1;
+					if(H_Cont == x1_search2)
+						LinhaCheck2 <= LinhaCheck2 + 1;
+					if(H_Cont >= x1_search && H_Cont <= x2_search) begin
+						if(H_Cont == x1_search)
+								contadorBranco2 <= 0;
+						if(morfologico == 10'b1111111111) begin
+							if(OkLinha == 0 && contadorBranco == 0) begin
+								x1_achou <= H_Cont;
+								y1_achou <= V_Cont;
+							end
+							contadorBranco <= contadorBranco + 1;
+							if(contadorBranco >= 240) begin
+								contadorBranco <= 0;
+								OkLinha <= OkLinha + 1;
+								if(OkLinha == 1) 
+									LinhaCheck <= OkLinha;
+							end
+							else
+								achou <= 1'b0;
+						end
+						if(OkLinha >= 40) begin
+							x1_achou <= x1_achou - 15;
+							y1_achou <= y1_achou - 10;
+							x2_achou <= H_Cont + 30;
+							y2_achou <= V_Cont + 10;
+							OkLinha <= 0;
+							achou <= 1'b1;
+						end
+					end
+					if(H_Cont >= x2_search2 && H_Cont <= x1_search2) begin
+						if(H_Cont == x2_search2)
+								contadorBranco <= 0;
+						if(morfologico == 10'b1111111111) begin
+							if(OkLinha2 == 0 && contadorBranco2 == 0) begin
+								x1_achou2 <= H_Cont;
+								y1_achou2 <= V_Cont;
+							end
+							contadorBranco2 <= contadorBranco2 + 1;
+							if(contadorBranco2 >= 240) begin
+								contadorBranco2 <= 0;
+								OkLinha2 <= OkLinha2 + 1;
+								if(OkLinha2 == 1) 
+									LinhaCheck2 <= OkLinha2;								
+							end
+							else
+								achou2 <= 1'b0;
+						end
+						if(OkLinha2 >= 40) begin
+							x1_achou2 <= x1_achou2 - 15;
+							y1_achou2 <= y1_achou2 - 10;
+							x2_achou2 <= H_Cont + 30;
+							y2_achou2 <= V_Cont + 10;
+							OkLinha2 <= 0;
+							achou2 <= 1'b1;
+						end
+					end
+					if(V_Cont >= V_SYNC_TOTAL && H_Cont == 0) begin
+						x1_search <= x1_search + 10;
+						x2_search <= x2_search + 10;
+						x1_search2 <= x1_search2 - 10;
+						x2_search2 <= x2_search2 - 10;
+						if(x1_search == x1_search2) begin
+							x1_search <= 0;
+							x2_search <= 300;
+							x1_search2 <= 800;
+							x2_search2 <= 500;
+						end
+					end
+				end
+				if(achou == 1'b1 || achou2 == 1'b1)
+					if(V_Cont == 0 && H_Cont == 1 && valido == 1'b0)
+							check <= 1'b1;
+				if(ativo == 1'b1)begin 
+					if(achou == 1'b1)begin 
 						if(H_Cont >= x1_achou && H_Cont <= x2_achou && V_Cont >= y1_achou	&& V_Cont <= y2_achou) begin
+							oVGA_R <= puroR;
+							oVGA_G <= puroG;
+							oVGA_B <= puroB;
+							check <= 1'b0;
+						end
+						else begin
+							oVGA_R <= mVGA_R;
+							oVGA_G <= mVGA_G;
+							oVGA_B <= mVGA_B;
+							check <= 1'b0;
+						end
+					end
+					else if(achou2 == 1'b1) begin
+						if(H_Cont >= x1_achou2 && H_Cont <= x2_achou2 && V_Cont >= y1_achou2	&& V_Cont <= y2_achou2) begin
 							oVGA_R <= puroR;
 							oVGA_G <= puroG;
 							oVGA_B <= puroB;
@@ -193,6 +305,37 @@ always@(posedge iCLK or negedge iRST_N)
 				oVGA_V_SYNC <= mVGA_V_SYNC;				
 			end               
 	end
+	
+	
+
+always@(posedge iCLK or negedge iRST_N) begin
+	if (!iRST_N)	begin
+		contador <= 0;
+		reset_busca <= 1'b0;
+		valido <= 1'b0;
+	end
+	else begin	
+		if(reset_validacao == 1'b1)
+			reset_busca <= 1'b0;
+		if(check == 1'b1) begin
+			if(H_Cont >= x1_achou2 && H_Cont <= x2_achou2 && V_Cont >= y1_achou2 && V_Cont <= y2_achou2) begin
+				if(bSobel == 10'b1111111111)
+					contador <= contador + 1;
+			end
+			if(V_Cont == y2_achou2 && H_Cont == x2_achou2) begin
+				if(contador <= 12000) begin
+					contador <= 0;
+					reset_busca <= 1'b1;
+				end
+				else begin
+					valido <= 1'b1;
+					contador <= 0;
+					reset_busca <= 0;
+				end
+			end
+		end
+	end
+end
 
 
 
